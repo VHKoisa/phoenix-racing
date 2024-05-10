b1307080a976055d225b801ba4174089d33d125c
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-05-10 19:36:35 +0530

    Added Padding in Navbar for all devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,19 +24,19 @@ function Header() {
   const [isOpen, setOpen] = useState(false);
   return (
     <>
-      <div className="bg-blue-950 sticky top-0 z-30 mx-auto w-full items-center justify-between flex">
+      <div className="bg-blue-950 sticky top-0 z-30 mx-auto px-2 w-full items-center justify-between flex">
         <div className="flex items-center my-2 mx-1">
           <Logo />
-          <div className="mx-5 text-white text-3xl font-serif">
+          <div className="mx-5 text-white text-2xl lg:text-3xl font-serif">
             PHOENIX RACING
           </div>
         </div>
 
         <nav className="flex lg:w-1/4">
-          <div className="hidden w-full justify-between lg:flex  border-2 border-white rounded-2xl px-8 py-2">
+          <div className="hidden w-full justify-around lg:flex  border-2 border-white rounded-2xl py-2">
             <NavLinks />
           </div>
-          <div className="lg:hidden">
+          <div className="lg:hidden ">
             <label htmlFor="burger" className="burger">
               <input type="checkbox" id="burger" onChange={()=> {burger.checked?setOpen(true):setOpen(false)} } />
               <span></span>
