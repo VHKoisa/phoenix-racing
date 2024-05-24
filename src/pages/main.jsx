c3d716587f4c5613497654f38e4ef7c95485e508
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-05-24 18:38:40 +0530

    Cars page added with 3D carousel animation

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,6 +4,7 @@ import Team from "/src/pages/team/Team.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./about/About";
+import Cars from "./cars/Cars";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/cars" element={<Cars />} />
         {/* <Route path="/" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
