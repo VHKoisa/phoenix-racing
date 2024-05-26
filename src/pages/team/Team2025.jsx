1993abc4e31f306675e4f8a526877c7175a61cc2
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-05-26 11:17:35 +0530

    Linked In links added in team cards' backside

--- src/pages/team/Team2025.jsx
+++ src/pages/team/Team2025.jsx
@@ -9,23 +9,32 @@ const Team2025 = () => {
       </div>
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
-          <Card photo="2025/Captain.jpg" name="Himanshu Singh" position="Captain" />
           <Card
+            linkedin="https://www.linkedin.com/in/himanshu-singh-00559b1b3/"
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
+            linkedin=""
             photo="2025/ViceCaptain.jpg"
             name="Prince Kansara"
             position="Vice-Captain"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/abhishek-yadav-8357a726a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/OperationalManager.jpg"
             name="Abhishek Yadav"
             position="Operational Manager"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/jeet-tank-0a4905256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/ManufacturingHead.jpg"
             name="Jeet Tank"
             position="Manufacturing Head"
           />
           <Card
+            linkedin=""
             photo="2025/ManufacturingHeadElectrical.jpg"
             name="Kaustuv"
             position="Manufacturing Head-Electrical"
@@ -39,26 +48,35 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin="https://www.linkedin.com/in/maahir-malde?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/CoChairperson.jpg"
             name="Maahir Malde"
             position="Co-Chairperson"
           />
           <Card
+            linkedin="www.linkedin.com/in/aryan-baviskar-1a245b2a9"
             photo="2025/CoChairperson2.jpg"
             name="Aryan Baviskar"
             position="Co-Chairperson"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/burhanuddin-dadawala-57b830288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/MarketingHead.jpg"
             name="Burhanuddin Dadawala"
             position="Marketing Head"
           />
           <Card
+            linkedin=""
             photo="2025/MarketingCoHead.jpg"
             name="Sri Vishal"
             position="Marketing Co-Head"
           />
-          <Card photo="2025/Treasurer.jpg" name="Devansh" position="Treasurer" />
+          <Card
+            linkedin=""
+            photo="2025/Treasurer.jpg"
+            name="Devansh"
+            position="Treasurer"
+          />
         </div>
       </div>
 
@@ -68,26 +86,35 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin="https://www.linkedin.com/in/vatsal-sachaniya-b52a13247?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/ChassisHOD.jpg"
             name="Vatsal Sachaniya"
             position="Head Of The Department"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/ninad-wadaskar-7b499830b"
             photo="2025/ChassisMember1.jpg"
             name="Ninad Wadaskar"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/ChassisMember2.jpg"
             name="Rabbit Sinwar"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/himanshu-singh-00559b1b3/"
             photo="2025/ChassisMember3.jpg"
             name="Himanshu Singh"
             position="Member"
           />
-          <Card photo="2025/ChassisMember4.jpg" name="Jeet Tank" position="Member" />
+          <Card
+            linkedin="https://www.linkedin.com/in/jeet-tank-0a4905256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            photo="2025/ChassisMember4.jpg"
+            name="Jeet Tank"
+            position="Member"
+          />
         </div>
       </div>
 
@@ -97,26 +124,31 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin=""
             photo="2025/SNSHOD.jpg"
             name="Aayushman Singh"
             position="Head Of The Department"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/swastik-c-a7b963216 "
             photo="2025/SNSMember1.jpg"
             name="Swastik Choudhary"
             position="Member"
           />
           <Card
+            linkedin="www.linkedin.com/in/aryan-baviskar-1a245b2a9"
             photo="2025/SNSMember2.jpg"
             name="Aryan Baviskar"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/abhishek-yadav-8357a726a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/SNSMember3.jpg"
             name="Abhishek Yadav"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/burhanuddin-dadawala-57b830288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/SNSMember4.jpg"
             name="Burhanuddin Dadawala"
             position="Member"
@@ -130,26 +162,31 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin="https://www.linkedin.com/in/ashok-dudi-0318a2256"
             photo="2025/PowertrainHOD.jpg"
             name="Ashok Dudi"
             position="Head Of The Department"
           />
           <Card
+            linkedin=""
             photo="2025/PowertrainMember1.jpg"
             name="Aashlesh Paridhi"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/allu-ajith-051a67291"
             photo="2025/PowertrainMember2.jpg"
             name="Ajith Shaikh"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/PowertrainMember3.jpg"
             name="Devansh"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/maahir-malde?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/PowertrainMember4.jpg"
             name="Maahir Malde"
             position="Member"
@@ -163,21 +200,25 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin="https://www.linkedin.com/in/hritik-raj-543aa430b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/BrakesHOD.jpg"
             name="Hritik Raj"
             position="Head Of The Department"
           />
           <Card
+            linkedin=""
             photo="2025/BrakesMember1.jpg"
             name="Anurag Parihar"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/BrakesMember2.jpg"
             name="Prateek Verma"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/BrakesMember3.jpg"
             name="Yashaswini Sharma"
             position="Member"
@@ -191,33 +232,49 @@ const Team2025 = () => {
       <div className="flex justify-center">
         <div className="flex flex-wrap justify-center w-full">
           <Card
+            linkedin=""
             photo="2025/ElectricalHOD.jpg"
             name="Kaustuv"
             position="Head Of The Department"
           />
-          <Card photo="2025/ElectricalMember1.jpg" name="Akshay" position="Member" />
           <Card
+            linkedin=""
+            photo="2025/ElectricalMember1.jpg"
+            name="Akshay"
+            position="Member"
+          />
+          <Card
+            linkedin="https://www.linkedin.com/in/hrisheek-paritala-24404525a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/ElectricalMember2.jpg"
             name="Hrishik"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/ElectricalMember3.jpg"
             name="Preetham"
             position="Member"
           />
           <Card
+            linkedin="https://www.linkedin.com/in/rohan-prajapati-b3232120b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             photo="2025/ElectricalMember4.jpg"
             name="Rohan Prajapati"
             position="Member"
           />
-          <Card photo="2025/ElectricalMember5.jpg" name="Vidhan" position="Member" />
           <Card
+            linkedin=""
+            photo="2025/ElectricalMember5.jpg"
+            name="Vidhan"
+            position="Member"
+          />
+          <Card
+            linkedin=""
             photo="2025/ElectricalMember6.jpg"
             name="Prince Kansara"
             position="Member"
           />
           <Card
+            linkedin=""
             photo="2025/ElectricalMember7.jpg"
             name="Sri Vishal"
             position="Member"
