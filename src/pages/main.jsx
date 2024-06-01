1e6b978c1f234263a544bb34702a25522989757c
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-06-01 18:16:20 +0530

    Comment removed in main.jsx

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,12 +4,9 @@ import Team from "/src/pages/team/Team.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./about/About";
-// <<<<<<< HEAD
 import Sponsors from "/src/pages/Sponsors/Sponsors.jsx";
-// =======
 import Cars from "./cars/Cars";
 import Sponsors from "./sponsors/Sponsors";
-// >>>>>>> cfcbdbe746be32a037f4236957d2c5e25feecaa5
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
