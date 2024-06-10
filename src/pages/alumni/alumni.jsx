46ba851c02c0ccb2d0fecd3a275ffa2b90b0a782
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-06-10 15:59:08 +0530

    alumni page set

--- src/pages/alumni/alumni.jsx
+++ src/pages/alumni/alumni.jsx
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
-import Card from "../team/Card"
-import './alumni.scss'
+import React, { Component } from "react";
+import Card from "../team/Card";
+import "./alumni.scss";
 import Header from "/src/components/Header";
 import Footer from "/src/components/Footer";
-
 
 export default class alumni extends Component {
   render() {
     return (
+      <div className="mainA">
+        <Header />
 
-        
-      <div className='mainA'>
-      <Header />
+        <h1>Alumni</h1>
+        <p>
+          Thank you for your continued support and for being an integral part of
+          our alumni family, We are deeply grateful for your contributions and
+          dedication to our community's success
+        </p>
 
-            
-                <h1>Alumni</h1>
-                <p>Thank you for your continued support and for being an integral part of our alumni family,
-                We are deeply grateful for your contributions and dedication to our community's success</p>
-               
-                <div className="line">
-                    
-                </div>
-                <div className="row">
-                <Card
+        <div className="line"></div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -39,20 +42,22 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
-                <div className="row">
-                <Card
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -69,20 +74,22 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
-                <div className="row">
-                <Card
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -99,20 +106,22 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
-                <div className="row">
-                <Card
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -129,20 +138,22 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
-                <div className="row">
-                <Card
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -159,20 +170,22 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
-                <div className="row">
-                <Card
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
+        <div className="row">
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+          <Card
             linkedin=""
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
@@ -189,22 +202,17 @@ export default class alumni extends Component {
             photo="2025/Captain.jpg"
             name="Himanshu Singh"
             position="Captain"
-          /><Card
-          linkedin=""
-          photo="2025/Captain.jpg"
-          name="Himanshu Singh"
-          position="Captain"
-        /><Card
-        linkedin=""
-        photo="2025/Captain.jpg"
-        name="Himanshu Singh"
-        position="Captain"
-      />
-                </div>
+          />
+          <Card
+            linkedin=""
+            photo="2025/Captain.jpg"
+            name="Himanshu Singh"
+            position="Captain"
+          />
+        </div>
 
-      <Footer />
-
+        <Footer />
       </div>
-    )
+    );
   }
 }
