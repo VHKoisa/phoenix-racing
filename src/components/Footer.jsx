62a5daccba99471522813f87a5b9627e8d2845f0
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-06-15 15:21:36 +0530

    Footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,122 +16,118 @@ const Footer = () => {
           </div>
           <div className="text-blue-800 text-2xl">SAE PHOENIX RACING</div>
         </div>
-        <div className="w-full p-5 lg:w-1/4 flex items-center justify-center mt-4">
+        <div className="w-full p-5 lg:w-1/4  flex justify-center">
           <div className="">
-            <div className="text-blue-800 text-3xl text-center">Follow Us</div>
-            <div className="flex flex-wrap justify-around text-center text-slate-600">
-              <a
-                className="size-10 lg:w-1/4 m-2"
-                href="https://www.facebook.com/phoenixracing360/"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="facebook-f"
-                  className="svg-inline--fa fa-facebook-f fa-xl me-4 p-1"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 400 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
-                  ></path>
-                </svg>
-              </a>
-              <a
-                className="size-10 lg:w-1/4 m-2"
-                href="https://www.instagram.com/phoenixracing.sae/"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="instagram"
-                  className="svg-inline--fa fa-instagram fa-xl me-4 p-1"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
-                  ></path>
-                </svg>
-              </a>
-              <a
-                className="size-10 lg:w-1/4 m-2"
-                href="https://www.linkedin.com/company/phoenix-racing-svnit/"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="linkedin-in"
-                  className="svg-inline--fa fa-linkedin-in fa-xl me-4 p-1"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                  ></path>
-                </svg>
-              </a>
-              <a
-                className="size-10 lg:w-1/4 m-2"
-                href="https://www.youtube.com/@FSAEPhoenixRacing"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="youtube"
-                  className="svg-inline--fa fa-youtube fa-xl me-4 p-1"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
-                  ></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="w-full p-5 lg:w-1/4  flex items-center justify-center">
-          <div className="">
-            <div className="text-blue-800 text-3xl text-center">Navigate</div>
-            <div className="text-slate-600 text-center">
-              <div>
-                {" "}
+            <div className="text-blue-800 text-4xl text-center">Navigate</div>
+            <div className="text-slate-600 text-lg text-center ">
+              <div className="p-1">
                 <NavLink to="/"> Home </NavLink>
               </div>
-              <div>
-                {" "}
+              <div className="p-1">
                 <NavLink to="/cars"> Cars </NavLink>
               </div>
-              <div>
-                {" "}
+              <div className="p-1">
                 <NavLink to="/team"> Team </NavLink>
               </div>
-              <div>
-                {" "}
+              <div className="p-1">
                 <NavLink to="/sponsors"> Sponsors </NavLink>
               </div>
-              <div>
-                {" "}
+              <div className="p-1">
                 <NavLink to="/alumni"> Alumni </NavLink>
               </div>
             </div>
           </div>
         </div>
+        <div className="w-full p-5 lg:w-1/4 flex items-center justify-center mt-4">
+          {/* <div className=""> */}
+          {/* <div className="text-blue-800 text-3xl text-center">Follow Us</div> */}
+          <div className="flex flex-wrap justify-around text-center text-slate-600">
+            <a
+              className="size-16 lg:w-1/3 m-2"
+              href="https://www.facebook.com/phoenixracing360/"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="facebook-f"
+                className="svg-inline--fa fa-facebook-f fa-xl me-4 p-1"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 400 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              className="size-16 lg:w-1/3 m-2"
+              href="https://www.instagram.com/phoenixracing.sae/"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="instagram"
+                className="svg-inline--fa fa-instagram fa-xl me-4 p-1"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              className="size-16 lg:w-1/3 m-2"
+              href="https://www.linkedin.com/company/phoenix-racing-svnit/"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="linkedin-in"
+                className="svg-inline--fa fa-linkedin-in fa-xl me-4 p-1"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              className="size-16 lg:w-1/3 m-2"
+              href="https://www.youtube.com/@FSAEPhoenixRacing"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="youtube"
+                className="svg-inline--fa fa-youtube fa-xl me-4 p-1"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          {/* </div> */}
+        </div>
+        
 
-        <div className="w-full p-5 lg:w-1/4 flex items-center justify-center">
+        {/* <div className="w-full p-5 lg:w-1/4 flex items-center justify-center">
           <div className="">
             <div className="text-blue-800 text-3xl text-center">Contact Us</div>
             <div className="text-slate-600 text-center">
@@ -212,6 +208,96 @@ const Footer = () => {
               </div>
             </div>
           </div>
+        </div> */}
+        <div className="w-full p-5 lg:w-1/4 flex items-center">
+        <div className="grid grid-cols-2 gap-4 text-slate-600 ">
+          {/* <div className=""> */}
+          {/* <div className="text-blue-800 text-3xl text-center">Contact Us</div> */}
+          {/* <div className="text-center"> */}
+          {/* <div className="flex justify-center pb-2"> */}
+          <div className="flex justify-end">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="envelope"
+              className="svg-inline--fa fa-envelope fa-lg w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+              ></path>
+            </svg>
+          </div>
+          <a href="mailto:sae@svnit.ac.in">sae@svnit.ac.in</a>
+          {/* </div> */}
+          {/* <div className="flex justify-center pb-2"> */}
+          <div className="flex justify-end">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="phone"
+              className="svg-inline--fa fa-phone fa-lg w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
+              ></path>
+            </svg>
+          </div>
+          <a href="tel:+918000770080">+919079880827</a>
+          {/* </div> */}
+          {/* <div className="flex items-start justify-around"> */}
+          <div className="flex justify-end">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="location-dot"
+              className="svg-inline--fa fa-location fa-lg w-6 h-fit"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
+              ></path>
+            </svg>
+          </div>
+          <a href="https://maps.app.goo.gl/PQowf9juSUjDwWSq7">
+            {/* <div className="text-left"> */}
+            IC Engine Lab, <br /> SVNIT Campus, <br />
+            Ichchhanath,
+            <br /> Surat, Gujarat,
+            <br /> India - 395007 &nbsp;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              className="float-right"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                d="M19 13v10H1V5h10m3-4h9v9m-13 4L23 1z"
+              />
+            </svg>
+            {/* </div> */}
+          </a>
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
+        </div>
         </div>
       </div>
     </>
