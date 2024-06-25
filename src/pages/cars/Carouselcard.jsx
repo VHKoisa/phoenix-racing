e57aea92c439d7c3c87d1d6cf0ca616b461c069a
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-06-25 19:04:32 +0530

    Solved the issue of overflowing text in GKDC

--- src/pages/cars/Carouselcard.jsx
+++ src/pages/cars/Carouselcard.jsx
@@ -5,7 +5,7 @@ const Carouselcard = (props) => {
   return (
     <><div className="border-white border-2 w-full h-full main"> 
         <img src={imgAddr} alt={props.img} id="carousel-img" className="w-full h-3/4" />
-        <div className="text-blue-950 h-1/4 bg-slate-300 flex flex-col justify-center">
+        <div className="text-white leading-5 lg:leading-6 h-1/4 flex flex-col justify-center" style={{backgroundColor: "#0072bb"}}>
           <div className="text-center text-xl font-bold">{props.name}</div>
           <div className="text-center">{props.year}</div>
           <div className="text-center">{props.text}</div>
