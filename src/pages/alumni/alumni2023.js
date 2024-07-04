ceb5bf230086f47fd91e703e43197369d9dc17bd
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-07-04 16:00:46 +0530

    data of 2023 mates for alumni page

---
+++ src/pages/alumni/alumni2023.js
@@ -0,0 +1,227 @@
+const team2023 = [
+    {
+      name: "Pratham Parmar",
+      position: "Head",
+      department: "Brakes",
+      section: "technical",
+      image:"https://drive.google.com/uc?id=1QmOYrJ4vnejXjoLnntQbTBcTna7VyL1n",
+      linkedIn: "https://www.linkedin.com/in/pratham-parmar-244022215",
+    },
+    {
+      name: "Hardik Choudhary",
+      position: "Head",
+      department: "Chasis",
+      section: "technical",
+      image:
+        "https://drive.google.com/uc?export=view&id=1gJqr5Lzy3VULe-J5MA65B3AtAy_9dX1W",
+      linkedIn: "https://www.linkedin.com/in/hardik-choudhary-38524324a",
+    },
+    {
+      name: "Parthiv Patel",
+      position: "Head",
+      department: "Powertrain",
+      section: "technical",
+      image:
+        "https://drive.google.com/uc?export=view&id=1xyWyiheMq7oEBl0CSx-RO5LWBi1peWxk",
+      linkedIn: "",
+    },
+    {
+      name: "Pratham Sharma",
+      position: "Head",
+      department: "Powertrain",
+      image:
+        "https://drive.google.com/uc?export=view&id=1SbT9mv1BaGNsEaSzaBs4ZWe9pNxxokU6",
+      linkedIn:
+        "https://www.linkedin.com/in/pratham-sharma-b8741024a?trk=contact-info",
+    },
+    {
+      name: "Vansh Jogia",
+      position: "Head",
+      department: "Sns",
+      section: "technical",
+      image:
+        "https://drive.google.com/uc?export=view&id=1vmm54KGoYS_Vh_qaYO9N_LHDGiRhZKYV",
+      linkedIn: "https://www.linkedin.com/in/vansh-jogia-238773216",
+    },
+    {
+      name: "S L Satyanarayana",
+      position: "Head",
+      department: "Electrical",
+      section: "technical",
+      image:
+        "https://drive.google.com/uc?export=view&id=1GEEjYLmVgipUqlKbIw8Aq6aQE5kbPObI",
+      linkedIn: "https://www.linkedin.com/in/slsatyanarayana",
+    },
+    {
+      name: "Alin Polra",
+      position: "Team Member",
+      department: "Electrical",
+      image:
+        "https://drive.google.com/uc?export=view&id=11KJVzQc_UcdOVeG7NZoAHDy63dq2kwjn",
+      linkedIn: "",
+    },
+    {
+      name: "Harsh Patel",
+      section: "technical",
+      sectionPosition: "Captain",
+      department: "Chasis",
+      position: "Team Member",
+      image:
+        "https://drive.google.com/uc?export=view&id=1B5SVHh8pDDxuZukRrALPY3Vzo77pmILK",
+      linkedIn: "https://www.linkedin.com/in/harsh-patel-b43b81191",
+    },
+    {
+      name: "Rushi Desai",
+      section: "technical",
+      sectionPosition: "Vice Captain",
+      department: "Sns",
+      position: "Team Member",
+      image:
+        "https://drive.google.com/uc?export=view&id=1bylbQ1VGkMt8WGsCMY0UGd3lXZJCvA4X",
+      linkedIn: "https://www.linkedin.com/in/rushi-desai-4988001ba/",
+    },
+    {
+      name: "Anish Shah",
+      section: "managerial",
+      sectionPosition: "Chairperson",
+      department: "Sns",
+      position: "Team Member",
+      image:
+        "https://drive.google.com/uc?export=view&id=1uDqVXa6oO5FwG_h2qwgCOJgmYSCgL7bC",
+      linkedIn: "https://www.linkedin.com/in/2002-anish-shah",
+    },
+    {
+      name: "Pooja Patel",
+      section: "managerial",
+      sectionPosition: "Co-Chairperson",
+      department: "Powertrain",
+      position: "Team Member",
+      image:
+        "https://drive.google.com/uc?export=view&id=1QaRGyaDrkCUQ7BsD5MbOD9ncCTrmO4eD",
+      linkedIn: "https://www.linkedin.com/in/pooja-patel-b08292217",
+    },
+    {
+      name: "Arnav Vaghela",
+      position: "Team Member",
+      department: "Sns",
+      image:
+        "https://drive.google.com/uc?export=view&id=167cpNP-6lNlJd9PIIrHdQQ_89R9eTwik",
+      linkedIn: "",
+    },
+    {
+      name: "Yatharth Kapadia",
+      section: "managerial",
+      sectionPosition: "Treasurer",
+      department: "Powertrain",
+      position: "Team Member",
+      image:
+        "https://drive.google.com/uc?export=view&id=1YdjUHUpA3-74iJZs3W7SCGlUqONy8A5m",
+      linkedIn: "https://www.linkedin.com/in/yatharth-kapadia-705784200",
+    },
+    {
+      name: "Burhanuddin Lokhandwala",
+      position: "Team Member",
+      department: "Electrical",
+      image:
+        "https://drive.google.com/uc?export=view&id=1CdIbyNoXN7IaT7t87K4xiTVphbmVtxCX",
+      linkedIn: "https://www.linkedin.com/in/burhanuddinlokhandwala04",
+    },
+    {
+      name: "Janvi Gohil",
+      position: "Team Member",
+      department: "Brakes",
+      image:
+        "https://drive.google.com/uc?export=view&id=1lIcQAnahIzDBM5hVLG-2unNjumQHhoAU",
+      linkedIn: "",
+    },
+    {
+      name: "Kartik Mohadarkar",
+      position: "Team Member",
+      department: "Powertrain",
+      image:
+        "https://drive.google.com/uc?export=view&id=1mNSsmEWfJVC4IkkL67lr-U_xCbQO3lhP",
+      linkedIn: "https://www.linkedin.com/in/kartik-m0390",
+    },
+    {
+      name: "Nishit Prajapati",
+      position: "Team Member",
+      department: "Powertrain",
+      image:
+        "https://drive.google.com/uc?export=view&id=1VGpljkuHu_PQq52io1eUAG4IOPZWndlo",
+      linkedIn: "",
+    },
+    {
+      name: "Miit Chetan Pabari",
+      position: "Team Member",
+      department: "Powertrain",
+      section: "managerial",
+      sectionPosition: "Public Relations Head",
+      image:
+        "https://drive.google.com/uc?export=view&id=19umjl2J26m3qRStpCKDGWgI625F5JJUp",
+      linkedIn: "www.linkedin.com/in/miit-pabari",
+    },
+    {
+      name: "Aaditya Nimkar",
+      position: "Team Member",
+      department: "Powertrain",
+      image:
+        "https://drive.google.com/uc?export=view&id=1zpR1Hm9dJqYVubpU7Zth7Pms-j_7TECz",
+      linkedIn: "www.linkedin.com/in/aaditya-nimkar-21424224a",
+    },
+    {
+      name: "Vishwa Menpara",
+      position: "Team Member",
+      department: "Chasis",
+      image:
+        "https://drive.google.com/uc?export=view&id=14LMU03P34ZE6Aa4HA9smcorDUlIY3LYj",
+      linkedIn: "https://www.linkedin.com/in/vishwa-menpara",
+    },
+    {
+      name: "Adarsh Kataktalware",
+      position: "Team Member",
+      department: "Brakes",
+      image:
+        "https://drive.google.com/uc?export=view&id=1d_zPYMYHMsYSSxqqicHvQvhFr9jQ-SqW",
+      linkedIn: "https://www.linkedin.com/in/adarsh-kataktalware-0803a8243",
+    },
+    {
+      name: "A. Hemanth Kumar",
+      position: "Team Member",
+      department: "Sns",
+      image:
+        "https://drive.google.com/uc?export=view&id=1DwMvdtuH4gOfvk3LYPHVEd7XleDNBEwJ",
+      linkedIn: "",
+    },
+    {
+      name: "Nayan Lakade",
+      position: "Team Member",
+      department: "Sns",
+      image:
+        "https://drive.google.com/uc?export=view&id=1nkdilWT5TkgLJU1DYn-rpt4xQtB0W4hE",
+      linkedIn: "https://www.linkedin.com/in/nayan-lakade-34279a1bb",
+    },
+    {
+      name: "Kunal Gupta",
+      position: "Team Member",
+      department: "Powertrain",
+      image:
+        "https://drive.google.com/uc?export=view&id=1z01wnGhZ4wDwlY8dzSbtycchOwXRAsOJ",
+      linkedIn: "",
+    },
+    {
+      name: "Shrujal Zala",
+      position: "Team Member",
+      department: "Sns",
+      image:
+        "https://drive.google.com/uc?export=view&id=1acT36ZuFVnw_i5fbGPnvbw03TmJNxTRG",
+      linkedIn: "www.linkedin.com/in/shrujal-zala-a20887201",
+    },
+    {
+      name: "Dhruv Chudasuma",
+      position: "Team Member",
+      department: "Chasis",
+      image:
+        "https://drive.google.com/uc?export=view&id=1xeMpUnhGfyay1SewAxrNQ7ApbfGndblb",
+      linkedIn: "https://www.linkedin.com/in/dhruv-chudasma-64828121b",
+    },
+  ];
